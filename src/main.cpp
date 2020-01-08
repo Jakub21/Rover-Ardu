c65bf966e83dcb7cfc22222b4fe4bd3f0e77bc1d
Jakub21 <jakub21@mail.com>
2020-01-08 23:16:31 +0100

    Initial Commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,56 @@
-#define BAUDRATE 9600
-#define LED_PIN 13
+#define BAUDRATE 115200
 
 #include <Arduino.h>
 
+#define SERIALBUFF_CAPACITY 64
+#define SERIALBUFF_CALL_MAX_LEN 16
+#define SERIALBUFF_SEPARATOR ';'
+#include "SerialBuffer.hpp"
+SerialBuffer sBuffer;
+
+#include "SerialTransmitter.hpp"
+SerialTrasmitter sTransmitter;
+
+#define PIN_TMPR_1 5
+
+float mapval(float x, float ol, float oh, float nl, float nh) {
+  float os = oh - ol;
+  float ns = nh - nl;
+  x -= ol;
+  return (x / os * ns) + nl;
+}
+
+
+/* FUNCTIONS CALLABLE FROM THE RASPBERRY */
+
+void measureTemperature() {
+  float tmprCelc = mapval(analogRead(PIN_TMPR_1), 438, 572, 18, 35);
+  sTransmitter.send(transmitKeys.TmprRaspberry, tmprCelc);
+}
+
+
+/* PROGRAM STRUCTURE */
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_PIN, OUTPUT);
   Serial.begin(BAUDRATE);
 }
 
+void execute() {
+  // Execute stuff based on call contents
+  int index = call[0];
+  int arg0 = call[1], arg1 = call[2], arg2 = call[3], arg3 = call[4];
+  switch(index) {
+    case 0: measureTemperature(/*arg0, arg1, arg2,arg3*/); break;
+    default: sTransmitter.transmit(transmitKeys.Error);
+  }
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available()) {
-    digitalWrite(LED_PIN, true);
-    Serial.read();
-  } else {
-    digitalWrite(LED_PIN, false);
+  if (sBuffer.readAvailable()) {
+    sBuffer.readSerial();
+  }
+  if (sBuffer.callAvailable()) {
+    sBuffer.nextCall();
+    execute();
   }
 }
